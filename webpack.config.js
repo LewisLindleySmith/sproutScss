cfa3b25b7769bd5d8a1f82f00fb777804a440c3c
Eyebonk <eyebonk@hotmail.co.uk>
2020-05-13 18:18:10 +1000

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 // Webpack uses this to work with directories
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // This is main configuration object.
 // Here you write different options and tell Webpack what to do
 module.exports = {
   // Path to your entry point. From this file Webpack will begin his work
   entry: {
-    app: resolve("./src/js/app.js"),
-    site: resolve("./src/scss/site.scss"),
+    app: "./src/js/app.js",
+    site: "./src/scss/site.scss",
   },
 
   // Path and filename of your result bundle.
@@ -16,6 +17,52 @@ module.exports = {
     path: path.resolve(__dirname, "build/js"),
     filename: "bundle.js",
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: "css-loader",
+          },
+          {
+            // Then we apply postCSS fixes like autoprefixer and minifying
+            loader: "postcss-loader",
+          },
+          {
+            // First we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "build/css/bundle.css",
+    }),
+  ],
 
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
