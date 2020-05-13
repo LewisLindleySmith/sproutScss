c9b0e5983e39e85be65ff52a8aa86dc097804b47
Eyebonk <eyebonk@hotmail.co.uk>
2020-05-13 17:53:27 +1000

    inital upload

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// Webpack uses this to work with directories
+const path = require("path");
+
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = {
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: {
+    app: resolve("./src/js/app.js"),
+    site: resolve("./src/scss/site.scss"),
+  },
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, "build/js"),
+    filename: "bundle.js",
+  },
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript
+  // minifying and other thing so let's set mode to development
+  mode: "development",
+};
