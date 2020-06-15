b4e12e648363de8ebc45441dd95e3baccb8f389e
Alex personal <eyebonk@hotmail.co.uk>
2020-06-15 18:28:03 +1000

    webpack set up and bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sproutScss
+
+## Run commands
+
+- Open project root in terminal
+- `npm run serve` - Watch the JS and SCSS files
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,74 @@
 // Webpack uses this to work with directories
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-// This is main configuration object.
+// This is the main configuration object.
 // Here you write different options and tell Webpack what to do
-module.exports = {
-  // Path to your entry point. From this file Webpack will begin his work
-  entry: {
-    app: "./src/js/app.js",
-    site: "./src/scss/site.scss",
-  },
+module.exports = (env) => {
+  const resolve = (src) => path.resolve(__dirname, src);
+  const config = {
+    // Path to your entry point. From this file Webpack will begin his work
+    entry: {
+      site: resolve("src/scss/site.scss"),
+      app: resolve("src/js/app.js"),
+    },
 
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
-  output: {
-    path: path.resolve(__dirname, "build/js"),
-    filename: "bundle.js",
-  },
+    // Path and filename of your result bundle.
+    // Webpack will bundle all JavaScript into this file
+    output: {
+      filename: "js/[name].min.js",
+      chunkFilename: "js/chunks/[name]-[chunkhash].js",
+      path: resolve("build/"),
+      publicPath: "/",
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        // Apply rule for .sass, .scss or .css files
-        test: /\.(sa|sc|c)ss$/,
-
-        // Set loaders to transform files.
-        // Loaders are applying from right to left(!)
-        // The first loader will be applied after others
-        use: [
-          {
-            // This loader resolves url() and @imports inside CSS
-            loader: "css-loader",
-          },
-          {
-            // Then we apply postCSS fixes like autoprefixer and minifying
-            loader: "postcss-loader",
-          },
-          {
-            // First we transform SASS to standard CSS
-            loader: "sass-loader",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
             options: {
-              implementation: require("sass"),
+              presets: ["@babel/preset-env"],
             },
           },
-        ],
-      },
+        },
+        {
+          // Apply rule for .sass, .scss or .css files
+          test: /\.(sa|sc|c)ss$/,
+
+          // Set loaders to transform files.
+          // Loaders are applying from right to left(!)
+          // The first loader will be applied after others
+          use: [
+            {
+              // After all CSS loaders we use plugin to do his work.
+              // It gets all transformed CSS and extracts it into separate
+              // single bundled file
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: "css-loader",
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "css/site.css",
+      }),
     ],
-  },
+  };
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "build/css/bundle.css",
-    }),
-  ],
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript
-  // minifying and other thing so let's set mode to development
-  mode: "development",
+  return config;
 };
