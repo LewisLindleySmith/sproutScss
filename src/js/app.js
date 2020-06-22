8a19e52b486d1580277fb5473b3e7b04f5426c47
LewisLindleySmith <fao-lewis-smith@hotmail.com>
2020-06-22 15:09:44 +0100

    nav html css start

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
 console.log("hello from app.js!!");
+
+//navBar
+
+var navbar = document.getElementById("stickyNav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+  console.log("scroll", window.pageYOffset, sticky);
+}
+
+window.onscroll = function () {
+  myFunction();
+};
